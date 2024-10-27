67fe54a6819eedb760568e9de632088ff40dad30
Glatzel <893016099@qq.com.com>
2024-10-27 14:04:46 +0800

    fix

--- src/test_a.py
+++ src/test_a.py
@@ -1,12 +1,18 @@
-# def foo(x):
-#     return x + 1
+def foo(x):
+    return x + 1
+
+
 def bar(x):
     return x + 1
-   
-# def foobar(x  ):
-#     return x  +3  
 
-# def test_foo( ): 
-#     assert foo( 3  ) == 5
-def test_bar():   
-    assert foo(3) == 4  
+
+def foobar(x):
+    return x + 3
+
+
+def test_foo():
+    assert foo(3) == 5
+
+
+def test_bar():
+    assert foo(3) == 4
